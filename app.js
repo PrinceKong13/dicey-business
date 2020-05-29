fea6b979c2a37c298231b4204a43bae22abeeec1
Will <wk1394@gmail.com>
2020-05-29 17:10:30 -0500

    can generate dice

---
+++ app.js
@@ -0,0 +1,28 @@
+let dieDiv = document.getElementById("dice-container");
+let allDice = [];
+class Die {
+  constructor() {
+    this.value = 0;
+    this.die = document.createElement("div");
+    dieDiv.appendChild(this.die);
+    this.die.classList.add("die");
+    this.roll();
+    allDice.push(this);
+  }
+
+  roll() {
+    this.value = Math.ceil(Math.random() * 6);
+    this.die.textContent = this.value;
+  }
+}
+
+let dieButton = document.getElementById("generateDie");
+dieButton.addEventListener("click", () => {
+  new Die();
+  console.log(allDice);
+});
+
+let rollButton = document.getElementById("rollDiceBtn");
+rollButton.addEventListener("click", () => {
+  allDice.forEach(this.roll());
+});
