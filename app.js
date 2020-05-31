a1dff11c73be33a0e4b3a4b2a33a8b52adb5d104
Will <wk1394@gmail.com>
2020-05-31 16:56:11 -0500

    double clicking a die now removes it

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 let dieDiv = document.getElementById("dice-container");
 let allDice = [];
 let diceSum = 0;
+let deletedDice = [];
+let dieId = 0;
+
 class Die {
-  constructor() {
+  constructor(id) {
+    this.id = id;
     this.value = 0;
     this.die = document.createElement("div");
     dieDiv.appendChild(this.die);
@@ -10,20 +14,33 @@ class Die {
     this.roll();
     allDice.push(this);
     this.die.addEventListener("click", () => this.roll())
+    this.die.addEventListener("dblclick", () => this.remove() )
   }
 
   roll() {
     this.value = Math.ceil(Math.random() * 6);
     this.die.textContent = this.value;
   }
+
+  remove() {
+    let targetDie = this;
+    event.target.remove();
+    allDice.forEach(function(item, index) {
+      if(item === targetDie) {
+       deletedDice = allDice.splice(index, 1);
+      }
+    })
+  }
 }
 
+//Adds a button that generates another die on the page
 let dieButton = document.getElementById("generateDie");
 dieButton.addEventListener("click", () => {
-  new Die();
-  console.log(allDice);
+  new Die(dieId);
+  ++dieId;
 });
 
+//Adds a button that rolls all the dice
 let rollButton = document.getElementById("rollDiceBtn");
 rollButton.addEventListener("click", () => {
   allDice.forEach(function(item) {
@@ -31,6 +48,7 @@ rollButton.addEventListener("click", () => {
   });
 });
 
+//Adds a button that totals up the value of all currently showing dice
 let addButton = document.getElementById("addButton");
 addButton.addEventListener("click", () => {
   diceSum = 0;
